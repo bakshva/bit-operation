6269f58b3f2d3ea4ebb9bcc435eb8a521d2ff7f7
Alena <avf.20052807@gmail.com>
2024-04-07 23:29:59 +0700

    Add generate number

--- ex1/main.c
+++ ex1/main.c
@@ -37,6 +37,26 @@ uint32_t decode_varint(const uint8_t **bufp)
     *bufp = cur;
     return value; // Возвращаем декодированное число
 }
+// Генерация случайного числа с различными диапазонами
+uint32_t generate_number()
+{
+    const int r = rand();
+    const int p = r % 100;
+    if (p < 90)
+    {
+        return r % 128;
+    }
+    if (p < 95)
+    {
+        return r % 16384;
+    }
+    if (p < 99)
+    {
+        return r % 2097152;
+    }
+    return r % 268435455;
+}
+
 void write_numbers_to_files()
 {
     FILE *uncompressed_file = fopen("uncompressed.dat", "wb");
